a3634fdb4901d62e189f618a37ee00bc239fae0b
Азат Алекбаев <azat@MacBook-Pro-2.local>
2018-03-29 12:10:06 +0300

    initial layout

---
+++ PushNotificationsHomework/DetailOfNotificationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailOfNotificationViewController.swift
+//  PushNotificationsHomework
+//
+//  Created by Азат Алекбаев on 29.03.2018.
+//  Copyright © 2018 Азат Алекбаев. All rights reserved.
+//
+
+import UIKit
+
+class DetailOfNotificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PushNotificationsHomework/MainNotificationsInformationTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainNotificationsInformationTableViewController.swift
+//  PushNotificationsHomework
+//
+//  Created by Азат Алекбаев on 29.03.2018.
+//  Copyright © 2018 Азат Алекбаев. All rights reserved.
+//
+
+import UIKit
+
+class MainNotificationsInformationTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+
+}
